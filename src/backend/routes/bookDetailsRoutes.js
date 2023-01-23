85e9942660d00f590dc4f3206665ed8b401a83f7
M-Hawk <12922@coderacademy.edu.au>
2023-01-23 17:34:05 +1000

    changed routes to receive functionality purely from controllers

--- src/backend/routes/bookDetailsRoutes.js
+++ src/backend/routes/bookDetailsRoutes.js
@@ -1,19 +1,14 @@
 const express = require("express")
 const router = express.Router()
+const { 
+  getBookDetails, 
+  setBookDetails, 
+  updateBookDetails, 
+  deleteBookDetails,
+ } = require("../controllers/bookDetailsController")
 
-// app.use(express.json()) // parses all api arguments as json
+router.route("/").get(getBookDetails).post(setBookDetails)
 
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "Got all books details" }) // res.send if above used ^
-})
-router.post("/", (req, res) => {
-  res.status(200).json({ message: "Set a books details" })
-})
-router.put("/:id", (req, res) => {
-  res.status(201).json({ message: `Update book details ${req.params.id}` })
-})
-router.delete("/:id", (req, res) => {
-  res.status(200).json({ message: `Delete a books details ${req.params.id}` })
-})
+router.route("/:id").put(updateBookDetails).delete(deleteBookDetails)
 
 module.exports = router

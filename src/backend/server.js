7f8236b1ba916bc0adf27de33c3bcc259dbddac7
M-Hawk <12922@coderacademy.edu.au>
2023-01-23 18:40:09 +1000

    imported error handler function from errorMiddleware

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,11 +1,17 @@
 const express = require("express")
 const dotenv = require("dotenv").config()
-const port = process.env.PORT || 5000
-
+const {errorHandler} = require("./middleware/errorMiddleware.js")
+const port = process.env.PORT || 8000
 const app = express()
 
+// Middleware
 app.use(express.json())
 
+// Define route first
 app.use("/api/bookdetails", require("./routes/bookDetailsRoutes"))
+
+app.use(errorHandler)
+
+// Listening on port
 
 app.listen(port, () => console.log(`Server started on port ${port}`))

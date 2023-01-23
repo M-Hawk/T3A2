d15ee28fc3462bcc174a7707f79c6233b9fbb9d1
M-Hawk <12922@coderacademy.edu.au>
2023-01-23 18:39:06 +1000

    added minor test error handler to setBookDetails controller, added asyncHandler wrapper on all controllers

--- src/backend/controllers/bookDetailsController.js
+++ src/backend/controllers/bookDetailsController.js
@@ -1,33 +1,36 @@
+const asyncHandler = require("express-async-handler")
+
 // @desc    Get book details
 // @route   GET /api/bookdetails
 // @access  Private
-const getBookDetails = (req, res) => {
+const getBookDetails = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get Book Details" })
-}
+})
 
 // @desc    Set book details
 // @route   POST /api/bookdetails
 // @access  Private
-const setBookDetails = (req, res) => {
+const setBookDetails = asyncHandler(async (req, res) => {
   if(!req.body.title) {
-    res.status(400).json({ message: "Please add the title key" })
+    res.status(400)
+    throw new Error("Please add the title key")
   }
   res.status(200).json({ message: "Set a books details" })
-}
+})
 
 // @desc    Update book details
 // @route   PUT /api/bookdetails/:id
 // @access  Private
-const updateBookDetails = (req, res) => {
+const updateBookDetails = asyncHandler(async (req, res) => {
   res.status(201).json({ message: `Update book details ${req.params.id}` })
-}
+})
 
 // @desc    Delete book details
 // @route   DELETE /api/bookdetails/:id
 // @access  Private
-const deleteBookDetails = (req, res) => {
+const deleteBookDetails = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete a books details ${req.params.id}` })
-}
+})
 
 // exported controller functions
 module.exports = {

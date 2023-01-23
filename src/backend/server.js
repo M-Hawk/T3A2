d596065cf0d367061a8290703b76430776768e3c
M-Hawk <12922@coderacademy.edu.au>
2023-01-23 17:35:13 +1000

    added middleware express.json()

--- src/backend/server.js
+++ src/backend/server.js
@@ -4,6 +4,8 @@ const port = process.env.PORT || 5000
 
 const app = express()
 
+app.use(express.json())
+
 app.use("/api/bookdetails", require("./routes/bookDetailsRoutes"))
 
 app.listen(port, () => console.log(`Server started on port ${port}`))

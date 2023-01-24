10f77fe9a9a549a2cb4938d8eb409257908afab0
M-Hawk <12922@coderacademy.edu.au>
2023-01-24 11:34:56 +1000

    changed 4space tabs to two space tabs

--- src/backend/models/bookDetailsModel.js
+++ src/backend/models/bookDetailsModel.js
@@ -1,23 +1,23 @@
 const mongoose = require("mongoose")
 const bookDetailsSchema = mongoose.Schema({
-    title: {
-        type: String,
-        required: [true, "Please add a book title value"]
-    },
-    author: {
-        type: String,
-        required: [true, "Please add a book author value"]
-    },
-    genre: {
-        type: String,
-        required: [true, "Please add a book genre value"]
-    },
-    description: {
-        type: String,
-        required: [true, "Please add a book description value"]
-    }
-}, {
-    timestamp: true
+  title: {
+    type: String,
+    required: [true, "Please add a book title value"]
+  },
+  author: {
+    type: String,
+    required: [true, "Please add a book author value"]
+  },
+  genre: {
+    type: String,
+    required: [true, "Please add a book genre value"]
+  },
+  description: {
+    type: String,
+    required: [true, "Please add a book description value"]
+  }
+  }, {
+  timestamp: true
 })
 
 module.exports = mongoose.model("bookDetails", bookDetailsSchema)

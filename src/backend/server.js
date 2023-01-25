15c21196df920f78f8a4c6ba450d48970c120193
Dayle Clarke <dayleclarke07@gmail.com>
2023-01-25 21:13:30 +1000

    Added user routes to server.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -14,6 +14,7 @@ app.use(express.json())
 
 // Define route first
 app.use("/api/bookdetails", require("./routes/bookDetailsRoutes"))
+app.use("/api/users", require("./routes/userRoutes"))
 
 app.use(errorHandler)
 

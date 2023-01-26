651f328afac0284dadb40c1a61272e33bbf2a1b4
Dayle Clarke <dayleclarke07@gmail.com>
2023-01-26 11:57:18 +1000

    Added registerUser and loginUser functions

--- src/backend/controllers/userController.js
+++ src/backend/controllers/userController.js
@@ -1,23 +1,85 @@
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcryptjs")
+const asyncHandler = require("express-async-handler")
+const User = require("../models/userModel")
 // @desc    Register new user
 // @route   POST /api/users
 // @access  Public
-const registerUser = (req, res) => {
-  res.json({ message: "Register User" })
-}
+const registerUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body
+
+  if(!username || !email || !password) {
+    res.status(400)
+    throw new Error("Please add all fields")
+  }
+
+  //Check if user email exists
+  const userEmailExists= await User.findOne({email})
+
+  if(userEmailExists) {
+    res.status(400)
+    throw new Error("Email already exists.")
+  }
+  //Check if username exists
+  const usernameExists= await User.findOne({username})
+
+  if(usernameExists) {
+    res.status(400)
+    throw new Error("Username already exists.")
+  }
+  // Hash password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  //Create User
+  const user = await User.create({
+    username,
+    email,
+    password: hashedPassword
+  })
+  
+  if(user) {
+    res.status(201).json({
+      _id: user._id,
+      username: user.username,
+      email: user.email
+    })
+  } 
+  else {
+    res.status(400)
+    throw new Error("Invalid user data")
+  }
+})
 
 // @desc    Authenticate a User
 // @route   POST /api/users/login
 // @access  Public
-const loginUser = (req, res) => {
-  res.json({ message: "Login User" })
-}
+const loginUser = asyncHandler(async (req, res) => {
+  const {username, password} = req.body
+
+  // Check for user email
+  const user = await User.findOne({ username })
+
+  if(user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+    })
+  }
+  else {
+    res.status(400)
+      throw new Error("Invalid credentials")
+
+  }
+})
 
 // @desc    Get user data
 // @route   GET /api/users/profile
 // @access  Public
-const getProfile = (req, res) => {
+const getProfile = asyncHandler(async(req, res) => {
   res.json({ message: "Display profile" })
-}
+})
 
 module.exports = { 
   registerUser,

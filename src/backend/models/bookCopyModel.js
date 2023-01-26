0d6f7284179dc46f4f74fa1069dff852fad66db3
Dayle Clarke <dayleclarke07@gmail.com>
2023-01-26 15:25:06 +1000

    Added bookCopyModel

---
+++ src/backend/models/bookCopyModel.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+const bookCopySchema = mongoose.Schema({
+  bookDetails:{ 
+    type: mongoose.Schema.Types.ObjectId,
+    // required: true,
+    ref: "BookDetails"
+  },
+  isAvailable:{
+    type: Boolean,
+    default: true, 
+  },
+  }, {
+  timestamp: true
+})
+
+module.exports = mongoose.model("bookDetails", bookDetailsSchema)

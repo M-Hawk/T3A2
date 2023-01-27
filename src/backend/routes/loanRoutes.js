4fed2adb4d8a4ee69318df1fe526e53275d57117
M-Hawk <12922@coderacademy.edu.au>
2023-01-27 16:02:34 +1000

    added admin function to admin only routes

--- src/backend/routes/loanRoutes.js
+++ src/backend/routes/loanRoutes.js
@@ -8,14 +8,16 @@ const {
   deleteLoan
  } = require("../controllers/loanController")
 
-const {protect} = require("../middleware/authMiddleware")
+ const { protect, admin } = require("../middleware/authMiddleware")
 
 // Update and delete routes
-router.route("/:id").put(protect, updateLoan).delete(protect, deleteLoan)
+
+router.route("/:id").put(protect, admin, updateLoan).delete(protect, admin, deleteLoan)
 
 // Get and create routes
-router.route("/").get(protect, getLoans).post(protect, setLoan)
-router.route("/getmyloans").get(protect, getOwnLoans)
+
+router.route("/").get(protect, admin, getLoans).post(protect, setLoan)
+router.route("/getmyloans").get(protect, admin, getOwnLoans)
 
 
 module.exports = router

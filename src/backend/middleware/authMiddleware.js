c60dc67f2daf4b863a1615abc7ed0c1a76a982b0
M-Hawk <12922@coderacademy.edu.au>
2023-01-27 15:57:29 +1000

    added admin authentication function for use in admin routes

--- src/backend/middleware/authMiddleware.js
+++ src/backend/middleware/authMiddleware.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken")
 const asyncHandler = require("express-async-handler")
-const User = require("../models/userModel")
+const UserModel = require("../models/userModel")
 
 const protect = asyncHandler(async(req, res, next) => {
   let token
@@ -14,7 +14,7 @@ const protect = asyncHandler(async(req, res, next) => {
       const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
 
       //Get user from the token
-      req.user = await User.findById(decoded.id).select("-password")
+      req.user = await UserModel.findById(decoded.id).select("-password")
 
       next()
     } 
@@ -30,4 +30,16 @@ const protect = asyncHandler(async(req, res, next) => {
   }
 })
 
-module.exports = { protect }
+const admin = asyncHandler(async(req, res, next) => {
+  const user = await UserModel.findById(req.user.id)
+  if(user.isAdmin === true) {
+    res.status(200),
+    next()
+  }
+  else {
+    res.status(401).json({ message: "You are not authorised to perform that action" })
+  }
+})
+
+
+module.exports = { protect, admin }

4c7663ed433f865f005bb72ca931232039e39910
Dayle Clarke <dayleclarke07@gmail.com>
2023-01-31 17:12:23 +1000

    increase font size of navbar

--- src/frontend/src/components/NavBar.jsx
+++ src/frontend/src/components/NavBar.jsx
@@ -6,20 +6,20 @@ const NavBar = () => {
   
   return (
     <nav class="navbar navbar-expand-lg bg-success bg-gradient">
-  <div class="container-fluid">
-    <a class="navbar-brand text-light" href="#">Wormreads</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav ms-auto">
-        <Link class="nav-link active" aria-current="page" to="/"><FaHome /> Home</Link>
-        <Link class="nav-link" to="/login"><FaSignInAlt /> Login</Link>
-        <Link class="nav-link" to="/register"><FaUser /> Register</Link>
-        <Link class="nav-link" to="/books"><FaBook /> Books</Link>
+      <div class="container-fluid">
+        <Link class="navbar-brand text-light" to="/">Wormreads</Link>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav ms-auto">
+            <Link class="nav-link" aria-current="page" to="/"><FaHome /> Home</Link>
+            <Link class="nav-link" to="/login"><FaSignInAlt /> Login</Link>
+            <Link class="nav-link" to="/register"><FaUser /> Register</Link>
+            <Link class="nav-link" to="/books"><FaBook /> Books</Link>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
 </nav>
 
   )

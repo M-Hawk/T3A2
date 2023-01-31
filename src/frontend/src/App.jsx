8dc2b8fa177fa4a51108cd9203a24fbef5188bba
M-Hawk <12922@coderacademy.edu.au>
2023-01-31 14:20:12 +1000

    added books page and supporting imports in other files, added css for book tiles and removed unused css

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header"
 import Dashboard from "./pages/Dashboard"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
+import Books from "./pages/Books"
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
               <Route path="/" element={<Dashboard />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
+              <Route path="/books" element={<Books />} />
             </Routes>
         </div>
       </Router>

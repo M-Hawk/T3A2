57abb10d79fd93f0ef1be2b21304e79bbdec5f90
M-Hawk <12922@coderacademy.edu.au>
2023-01-31 21:02:37 +1000

    rearanged books page for basic layout to add mapping

--- src/frontend/src/pages/Books.jsx
+++ src/frontend/src/pages/Books.jsx
@@ -6,89 +6,73 @@ const GET_BOOKS_URL = "api/bookdetails"
 
 const Books = () => {
 
-
-  // const [book, setBook] = useState(() => {
-  //   return {
-  //     title: book ? book.title : "",
-  //     author: book ? book.author : "",
-  //     genre: book ? book.genre : "",
-  //     description: book ? book.description : "",
-  //   }
-  // })
+  const [books, setBooks] = useState([])
   
   useEffect(() => {
     const fetchBooks = async () => {
       try {
         const response = await axios.get(GET_BOOKS_URL)
-        const books = response.data
-        console.log(books)
+        const book = response.data.bookDetails
+        // console.log(JSON.stringify(response.data))
+        setBooks(book)
+        console.log(book)
       }
       catch (err){
         console.log(err.stack)
       }
+      // return(books)
     }
     fetchBooks()
   }, [])
 
+  
   return (
-    <section className="book-list">
-      
-      <Card style={{ width: '18rem' }} className="book">
-        <Card.Body>
-          <Card.Title className="book-title">Title</Card.Title>
-          <div className="book-details">
-            <div>Author: author</div>
-            <div>Genre: genre</div>
-            <div>Description: description</div>
-          </div>
-          <div className="book-button">
-            <Button variant="danger" onClick={() => handleRemoveBook(id)}>
-              Delete
-            </Button>{' '}
-            <Button variant="primary" onClick={() => history.push(`/edit/${id}`)}>
-              Edit
-            </Button>
-          </div>
-        </Card.Body>
-      </Card>
+    <>
+    <p>Hello</p>
+    <div>
+    {books.map((book) => {
+        <ul key ={book._id}>
+          <li>Author: {book.author}</li>
+        </ul>
+    })}
+    </div>
 
-      <Card style={{ width: '18rem' }} className="book">
-        <Card.Body>
-          <Card.Title className="book-title">Title</Card.Title>
-          <div className="book-details">
-            <div>Author: author</div>
-            <div>Genre: genre</div>
-            <div>Description: description</div>
-          </div>
-          <div className="book-button">
-            <Button variant="danger" onClick={() => handleRemoveBook(id)}>
-              Delete
-            </Button>{' '}
-            <Button variant="primary" onClick={() => history.push(`/edit/${id}`)}>
-              Edit
-            </Button>
-          </div>
-        </Card.Body>
-      </Card>
-
-    </section>
+    </>
   )
 }
 
-/* <Card style={{ width: '18rem' }} className="book">
-<Card.Body>
-  <Card.Title className="book-title">{title}</Card.Title>
-  <div className="book-details">
-    <div>Author: {author}</div>
-    <div>Genre: {genre} </div>
-    <div>Description: {description} </div>
-  </div>
-  <Button variant="danger" onClick={() => handleRemoveBook(id)}>
-    Delete
-  </Button>{' '}
-  <Button variant="primary" onClick={() => history.push(`/edit/${id}`)}>
-    Edit
-  </Button>
-</Card.Body>
-</Card> */
+// {books.length ? (
+//   ) : (
+//     <p style={{ marginTop: '2rem' }}>Your list is empty.</p>      )}
+
+{/* 
+<section className="book-list">
+  <Card style={{ width: '18rem' }} className="book">
+      <Card.Body>
+        <Card.Title className="book-title">{book.title}</Card.Title>
+        <div className="book-details">
+          <div>Author: {book.author}</div>
+          <div>Genre: {book.genre}</div>
+          <div>Description: {book.description}</div>
+        </div>
+        <div className="book-button">
+          <Button variant="danger" onClick={() => handleRemoveBook(id)}>
+            Delete
+          </Button>{' '}
+          <Button variant="primary" onClick={() => history.push(`/edit/${id}`)}>
+            Edit
+          </Button>
+        </div>
+      </Card.Body>
+  </Card> */}
+
+// const [books, setBooks] = useState(() => {
+//   return {
+//     title: setBooks ? books.title : "",
+//     author: setBooks ? books.author : "",
+//     description: setBooks ? books.description : "",
+//     genre: setBooks ? books.genre : "",
+//   }
+// })
+
 export default Books

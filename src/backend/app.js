f62ead6113d280a10c665e7e574771d8c38ca6ca
Dayle Clarke <dayleclarke07@gmail.com>
2023-02-03 07:32:47 +1000

    Modularised app.js and server.js to set up testing environment

---
+++ src/backend/app.js
@@ -0,0 +1,33 @@
+//Seperate file to allow automated testing to occur without running the server and listens on the port. Test framework can sent requests without going through the network.  
+const express = require("express")
+const colors = require("colors")
+const dotenv = require("dotenv").config()
+const {errorHandler} = require("./middleware/errorMiddleware.js")
+const connectDB =  require("./config/db")
+const cors = require("cors")
+
+// const scheduledFunctions = require("./scheduledFunctions")
+
+connectDB()
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.json())
+
+// Scheduled jobs
+
+// Checks 
+// scheduledFunctions.initScheduledJobs()
+// Middleware
+
+// Define route first
+app.use("/api/bookdetails", require("./routes/bookDetailsRoutes"))
+app.use("/api/users", require("./routes/userRoutes"))
+app.use("/api/bookcopies", require("./routes/bookCopyRoutes"))
+app.use("/api/loans", require("./routes/loanRoutes"))
+
+app.use(errorHandler)
+
+module.exports = app

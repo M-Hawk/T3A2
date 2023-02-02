bbb66cf80c79a4b18d045f5c98e80c55396107b8
M-Hawk <12922@coderacademy.edu.au>
2023-02-02 11:59:27 +1000

    added url's for book images to books page commented out

--- src/frontend/src/pages/Books.jsx
+++ src/frontend/src/pages/Books.jsx
@@ -24,10 +24,24 @@ export const randBookImg = () => {
   let randomImg=imgUrls[random]
   return randomImg
 }
+
+// Book images 
+// alice in wonderland = https://covers.openlibrary.org/b/isbn/9781841353463-M.jpg
+// pride and prejudice = https://covers.openlibrary.org/b/olid/OL40233763M-M.jpg
+// frankenstein = https://covers.openlibrary.org/b/olid/OL14448179M-M.jpg
+// the count of monte cristo = https://covers.openlibrary.org/b/olid/OL28434684M-M.jpg
+// david copperfield = https://covers.openlibrary.org/b/olid/OL44248875M-M.jpg
+// moby dick = https://covers.openlibrary.org/b/olid/OL29349220M-M.jpg
+// les mis = https://covers.openlibrary.org/b/olid/OL27211583M-M.jpg
+// dracula = https://covers.openlibrary.org/b/olid/OL31937676M-M.jpg
+// virginia wolf = https://covers.openlibrary.org/b/olid/OL43705038M-M.jpg
+// the sun also rises = https://covers.openlibrary.org/b/olid/OL974460M-M.jpg
+
+
 const Books = () => {
 
   const [books, setBooks] = useState([])
-  const [bookCopies, setBookCopies] = useState([])
+  // const [bookCopies, setBookCopies] = useState([])
 
   useEffect(() => {
     const fetchBooks = async () => {
@@ -61,13 +75,12 @@ const Books = () => {
   //   // Change this below to accept changes when new books are added or deleted
   // }, [books])
 
-  
+
   return (
     <>
       {books.length ? (
         <section className="book-list">
           {books.map((book) => 
-
             <Card key={book._id} bg="light" style={{ width: '18rem' }} className="book">
               <Card.Img variant="top" className="book-image" src= {randBookImg()} />
               <Card.Body>

a2ef677fe9560ba3fb791617088de04ca5052377
Dayle Clarke <dayleclarke07@gmail.com>
2023-02-03 11:02:46 +1000

    Ran basic testing on the getBookDetails route

--- src/backend/app.test.js
+++ src/backend/app.test.js
@@ -1,6 +1,19 @@
 const app = require("./app.js")
 const request = require("supertest")
 
+const {connectDB, disconnectDB} =  require("./config/db")
+
+// set up before-tests and after-tests operations
+beforeEach(async () => {
+  await connectDB()
+})
+
+afterEach(async () => {
+  await disconnectDB()
+})
+
+// then run tests
+
 describe("Home Page Test",() => {
   test("GET /", async() => {
     const res = await request(app).get("/")
@@ -8,5 +21,29 @@ describe("Home Page Test",() => {
     expect(res.headers['content-type']).toBe('application/json; charset=utf-8')
     expect(res.body.info).toBeDefined()
     expect(res.body.info).toBe("Wormreads API")   
+  })
+})
+
+describe("BookDetails Test",() => {
+  let res
+
+  beforeEach(async() => {
+    res = await request(app).get("/api/bookdetails")
+    expect(res.status).toBe(200)
+    expect(res.headers['content-type']).toMatch(/json/i) 
+  })
+  it("Should return an array", () => {   
+    expect(res.body).toBeInstanceOf(Array)   
+  })
+  it("Has an element with the correct data structure", () => {
+    const el = res.body[0]
+    expect(el._id).toBeDefined()
+    expect(el.title).toBeDefined()
+    expect(el.author).toBeDefined()
+    expect(el.genre).toBeDefined()
+    expect(el.description).toBeDefined()
+    expect(el.imageList).toBeDefined()
+    expect(el.imageDetailed).toBeDefined()
+    expect(el._id.length).toBe(24)
   })
 })

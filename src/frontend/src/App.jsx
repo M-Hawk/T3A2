dcd2f4e4e443fec68510cadba33f61dbb32a6045
Dayle Clarke <dayleclarke07@gmail.com>
2023-02-04 07:58:50 +1000

    Added add new book button to the books page

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -70,7 +70,7 @@ const App = () => {
               <Route path="/userprofile/:id" element={<UserProfile token={token} user={user}/>} />
               <Route path="/books" element={<Books user={user}/>} />
               <Route path="/books/:id" element={<BookInfo user={user}/>} />
-              <Route path="/books/add/details" element={<AddBookDetails />} />
+              <Route path="/books/add/" element={<AddBookDetails />} />
               <Route path="/books/add/copy/:details" element={<AddBookCopy />} />
             </Routes>
             </div>
--- src/frontend/src/pages/AddBookDetails.jsx
+++ src/frontend/src/pages/AddBookDetails.jsx
@@ -1,6 +1,7 @@
 import { useRef, useState, useEffect } from "react"
 import { FaUser, FaInfoCircle, FaCheck, FaTimes} from "react-icons/fa"
 import axios from "../apiConnect/axios"
+import { Link, useNavigate } from "react-router-dom"
 
 const TITLE_REGEX = /^[A-Za-z0-9\s\-_,\.;:@&()]{3,250}$/
 const AUTHOR_REGEX = /^[A-Za-z0-9\s\-_,\.;:@&()]{3,250}$/
@@ -12,6 +13,7 @@ const ADD_BOOK_DETAILS_URL = "/api/bookdetails"
 const AddBookDetails = ({book}) => {
   const titleRef = useRef()
   const errRef = useRef()
+  const navigateTo = useNavigate()
 
   const [title, setTitle] = useState("") 
   const [validTitle, setValidTitle] = useState(false)
@@ -76,7 +78,7 @@ const AddBookDetails = ({book}) => {
       // Delete these console logs after
       console.log(response.data)
       console.log(JSON.stringify(response))
-      setSuccess(true)
+      navigateTo("/books")
     } 
     catch (err) {
       if (!err?.response) {
@@ -94,160 +96,140 @@ const AddBookDetails = ({book}) => {
 
   return (
     <>
-      {success ? (
-        <section>
-          <h1>Book Successfully Added!</h1>
-          <p>
-            <a href="#">Sign In</a>
-          </p>
-        </section>   
-      ):(
-        <>
-        <section>
-          <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
-        </section>
-        <section className="heading">
-            <h2>
-              <FaUser /> Add New Book Details
-            </h2>
-            <p>Please enter the book details associated with the book you would like to add</p>
-        </section>
-        <section className="form">
-          <form onSubmit={handleSubmit}>
-            {/* Title Section */}
-            <div className="form-group">
-              <label htmlFor="title">
-                Title:
-                <span className={validTitle ? "valid": "hide"}>
-                  <FaCheck />
-                </span> 
-                <span className={validTitle || !title ? "hide" : "invalid"}>
-                  <FaTimes />
-                </span> 
-              </label>
-              <input 
-                type="text"
-                id="title"
-                ref= {titleRef}
-                onChange={(e) => setTitle(e.target.value)}
-                required
-                value={title}
-                placeholder="Enter the book's title"
-                aria-invalid={validTitle ? "false" : "true"}
-                aria-describedby="titlenote"
-                onFocus={() => setTitleFocus(true)}
-                onBlur={() => setTitleFocus(false)}
-              />
-              <p id="titlenote" className={titleFocus && title && !validTitle ? "instructions" : "offscreen"}>
-                <FaInfoCircle />
-                Please enter a valid title. <br />
-                3 to 250 characters.<br />
-                Letters, numbers, spaces, punctuation and the following symbols are allowed: @ &.
-              </p>
-            </div>
-            {/* Author Section */}
-            <div className="form-group">
-              <label htmlFor="author">
-                Author:
-                <span className={validAuthor ? "valid": "hide"}>
-                  <FaCheck />
-                </span> 
-                <span className={validAuthor || !author ? "hide" : "invalid"}>
-                  <FaTimes />
-                </span> 
-              </label>
-              <input 
-                type="text"
-                id="author"
-                onChange={(e) => setAuthor(e.target.value)}
-                required
-                placeholder="Enter the author."
-                aria-invalid={validAuthor ? "false" : "true"}
-                aria-describedby="authornote"
-                onFocus={() => setAuthorFocus(true)}
-                onBlur={() => setAuthorFocus(false)}
-              />
-              <p id="authornote" className={authorFocus && author && !validAuthor ? "instructions" : "offscreen"}>
-                <FaInfoCircle />
-                Please enter a valid author. <br />
-                3 to 250 characters.<br />
-                Letters, numbers, spaces, punctuation and the following symbols are allowed: @ &.
-              </p>
-                </div>
-            {/* Genre Section */}
-            <div className="form-group">
-              <label htmlFor="genre">
-                Genre:
-                <span className={validGenre ? "valid": "hide"}>
-                  <FaCheck />
-                </span> 
-                <span className={validGenre || !genre ? "hide" : "invalid"}>
-                  <FaTimes />
-                </span> 
-              </label>
-              <input 
-                type="text"
-                id="genre"
-                onChange={(e) => setGenre(e.target.value)}
-                required
-                placeholder="Enter the genre."
-                aria-invalid={validGenre ? "false" : "true"}
-                aria-describedby="genrenote"
-                onFocus={() => setGenreFocus(true)}
-                onBlur={() => setGenreFocus(false)}
-              />
-              <p id="genrenote" className={genreFocus && genre && !validGenre ? "instructions" : "offscreen"}>
-                <FaInfoCircle />
-                Please enter a valid genre. <br />
-                3 to 250 characters.<br />
-                Letters, numbers, spaces, punctuation and the following symbols are allowed: @ &.
-              </p>
-            </div>
-            {/* Description Section */}
-            <div className="form-group">
-              <label htmlFor="description">
-                Description:
-                <span className={validDescription ? "valid": "hide"}>
-                  <FaCheck />
-                </span> 
-                <span className={validDescription || !description ? "hide" : "invalid"}>
-                  <FaTimes />
-                </span> 
-              </label>
-              <input 
-                type="text"
-                id="description"
-                onChange={(e) => setDescription(e.target.value)}
-                required
-                placeholder="Enter the book's description"
-                aria-invalid={validDescription ? "false" : "true"}
-                aria-describedby="descriptionnote"
-                onFocus={() => setDescriptionFocus(true)}
-                onBlur={() => setDescriptionFocus(false)}
-              />
-              <p id="descriptionnote" className={descriptionFocus && description && !validDescription ? "instructions" : "offscreen"}>
-                <FaInfoCircle />
-                Please enter a valid description. <br />
-                Letters, numbers, spaces, punctuation and the following symbols are allowed: @ &.
-              </p>
-            </div>
-              {/* Fix formatting so that the button is disabled */}
-              <button className="btn btn-block">Add Book Details</button>
-              </form>
-            
-              <p>
-              Does the book you want to add already have details recorded in the system?<br />
-                <span className="line">
-                  {/* Put Router Link Here */}
-                  <a href="#">Add a Book Copy</a>
-                </span>
-              </p>
-            </section>
-          </> 
-        )}
-      </>
-  )
-}
+      <section>
+        <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+      </section>
+      <section className="heading">
+          <h2>
+            <FaUser /> Add New Book Details
+          </h2>
+          <p>Please enter the book details associated with the book you would like to add</p>
+      </section>
+      <section className="form">
+        <form onSubmit={handleSubmit}>
+          {/* Title Section */}
+          <div className="form-group">
+            <label htmlFor="title">
+              Title:
+              <span className={validTitle ? "valid": "hide"}>
+                <FaCheck />
+              </span> 
+              <span className={validTitle || !title ? "hide" : "invalid"}>
+                <FaTimes />
+              </span> 
+            </label>
+            <input 
+              type="text"
+              id="title"
+              ref= {titleRef}
+              onChange={(e) => setTitle(e.target.value)}
+              required
+              value={title}
+              placeholder="Enter the book's title"
+              aria-invalid={validTitle ? "false" : "true"}
+              aria-describedby="titlenote"
+              onFocus={() => setTitleFocus(true)}
+              onBlur={() => setTitleFocus(false)}
+            />
+            <p id="titlenote" className={titleFocus && title && !validTitle ? "instructions" : "offscreen"}>
+              <FaInfoCircle />
+              Please enter a valid title. <br />
+              3 to 250 characters.<br />
+              Letters, numbers, spaces, punctuation and the following symbols are allowed: @ &.
+            </p>
+          </div>
+          {/* Author Section */}
+          <div className="form-group">
+            <label htmlFor="author">
+              Author:
+              <span className={validAuthor ? "valid": "hide"}>
+                <FaCheck />
+              </span> 
+              <span className={validAuthor || !author ? "hide" : "invalid"}>
+                <FaTimes />
+              </span> 
+            </label>
+            <input 
+              type="text"
+              id="author"
+              onChange={(e) => setAuthor(e.target.value)}
+              required
+              placeholder="Enter the author."
+              aria-invalid={validAuthor ? "false" : "true"}
+              aria-describedby="authornote"
+              onFocus={() => setAuthorFocus(true)}
+              onBlur={() => setAuthorFocus(false)}
+            />
+            <p id="authornote" className={authorFocus && author && !validAuthor ? "instructions" : "offscreen"}>
+              <FaInfoCircle />
+              Please enter a valid author. <br />
+              3 to 250 characters.<br />
+              Letters, numbers, spaces, punctuation and the following symbols are allowed: @ &.
+            </p>
+              </div>
+          {/* Genre Section */}
+          <div className="form-group">
+            <label htmlFor="genre">
+              Genre:
+              <span className={validGenre ? "valid": "hide"}>
+                <FaCheck />
+              </span> 
+              <span className={validGenre || !genre ? "hide" : "invalid"}>
+                <FaTimes />
+              </span> 
+            </label>
+            <input 
+              type="text"
+              id="genre"
+              onChange={(e) => setGenre(e.target.value)}
+              required
+              placeholder="Enter the genre."
+              aria-invalid={validGenre ? "false" : "true"}
+              aria-describedby="genrenote"
+              onFocus={() => setGenreFocus(true)}
+              onBlur={() => setGenreFocus(false)}
+            />
+            <p id="genrenote" className={genreFocus && genre && !validGenre ? "instructions" : "offscreen"}>
+              <FaInfoCircle />
+              Please enter a valid genre. <br />
+              3 to 250 characters.<br />
+              Letters, numbers, spaces, punctuation and the following symbols are allowed: @ &.
+            </p>
+          </div>
+          {/* Description Section */}
+          <div className="form-group">
+            <label htmlFor="description">
+              Description:
+              <span className={validDescription ? "valid": "hide"}>
+                <FaCheck />
+              </span> 
+              <span className={validDescription || !description ? "hide" : "invalid"}>
+                <FaTimes />
+              </span> 
+            </label>
+            <input
+              type="text"
+              id="description"
+              onChange={(e) => setDescription(e.target.value)}
+              required
+              placeholder="Enter the book's description"
+              aria-invalid={validDescription ? "false" : "true"}
+              aria-describedby="descriptionnote"
+              onFocus={() => setDescriptionFocus(true)}
+              onBlur={() => setDescriptionFocus(false)}
+            />
+            <p id="descriptionnote" className={descriptionFocus && description && !validDescription ? "instructions" : "offscreen"}>
+              <FaInfoCircle />
+              Please enter a valid description. <br />
+              Letters, numbers, spaces, punctuation and the following symbols are allowed: @ &.
+            </p>
+          </div>
+          <button className="btn btn-block">Add Book Details</button>
+        </form>
+      </section>
+    </> 
+  )}
+
 
 
 export default AddBookDetails

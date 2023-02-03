dd0c485ec2231bef567dae260f7f9e620f7226f7
M-Hawk <12922@coderacademy.edu.au>
2023-02-03 21:58:55 +1000

    made some unworking changes to borrow button

--- src/backend/app.test.js
+++ src/backend/app.test.js
@@ -49,27 +49,27 @@ describe("App tests",() => {
       })
     })
   })
- test("Create new Book Details", async () =>{
-    const res = await request(app).post("/api/bookdetails").send({
-      title: "The Wonderful Wizard of Oz", 
-      author: "L. Frank Baum and W. W. Denslow",
-      genre: "Adventure",
-      description: "Folklore, legends, myths and fairy tales have followed childhood through the ages, for every healthy youngster has a wholesome and instinctive love for stories fantastic, marvelous and manifestly unreal. The winged fairies of Grimm and Andersen have brought more happiness to childish hearts than all other human creations. Yet the old time fairy tale, having served for generations, may now be classed as “historical” in the children's library; for the time has come for a series of newer “wonder tales” in which the stereotyped genie, dwarf and fairy are eliminated, together with all the horrible and blood-curdling incidents devised by their authors to point a fearsome moral to each tale.",
-      imageList: "https://covers.openlibrary.org/b/olid/OL7170815M-M.jpg",
-      imageDetailed: "https://covers.openlibrary.org/b/olid/OL7170815M-L.jpg"
-    })
-    expect(res.headers['content-type']).toMatch(/json/i) 
-    expect(res.body._id).toBeDefined()
-    expect(res.body.title).toBeDefined()
-    expect(res.body.title).toBe("The Wonderful Wizard of Oz")
-    expect(res.body.author).toBeDefined()
-    expect(res.body.author).toBe("L. Frank Baum and W. W. Denslow")
-    expect(res.body.genre).toBeDefined()
-    expect(res.body.genre).toBe("Adventure")
-    expect(res.body.description).toBeDefined()
-    expect(res.body.imageList).toBeDefined()
-    expect(res.body.imageList).toBe("https://covers.openlibrary.org/b/olid/OL7170815M-M.jpg")
-    expect(res.body.imageDetailed).toBeDefined()
-    expect(res.body.imageDetailed).toBe("https://covers.openlibrary.org/b/olid/OL7170815M-L.jpg")
-  })
+//  test("Create new Book Details", async () =>{
+//     const res = await request(app).post("/api/bookdetails").send({
+//       title: "The Wonderful Wizard of Oz", 
+//       author: "L. Frank Baum and W. W. Denslow",
+//       genre: "Adventure",
+//       description: "Folklore, legends, myths and fairy tales have followed childhood through the ages, for every healthy youngster has a wholesome and instinctive love for stories fantastic, marvelous and manifestly unreal. The winged fairies of Grimm and Andersen have brought more happiness to childish hearts than all other human creations. Yet the old time fairy tale, having served for generations, may now be classed as “historical” in the children's library; for the time has come for a series of newer “wonder tales” in which the stereotyped genie, dwarf and fairy are eliminated, together with all the horrible and blood-curdling incidents devised by their authors to point a fearsome moral to each tale.",
+//       imageList: "https://covers.openlibrary.org/b/olid/OL7170815M-M.jpg",
+//       imageDetailed: "https://covers.openlibrary.org/b/olid/OL7170815M-L.jpg"
+//     })
+//     expect(res.headers['content-type']).toMatch(/json/i) 
+//     expect(res.body._id).toBeDefined()
+//     expect(res.body.title).toBeDefined()
+//     expect(res.body.title).toBe("The Wonderful Wizard of Oz")
+//     expect(res.body.author).toBeDefined()
+//     expect(res.body.author).toBe("L. Frank Baum and W. W. Denslow")
+//     expect(res.body.genre).toBeDefined()
+//     expect(res.body.genre).toBe("Adventure")
+//     expect(res.body.description).toBeDefined()
+//     expect(res.body.imageList).toBeDefined()
+//     expect(res.body.imageList).toBe("https://covers.openlibrary.org/b/olid/OL7170815M-M.jpg")
+//     expect(res.body.imageDetailed).toBeDefined()
+//     expect(res.body.imageDetailed).toBe("https://covers.openlibrary.org/b/olid/OL7170815M-L.jpg")
+//   })
 })

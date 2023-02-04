c52d73c95ef7e8aaf3e16520c434651d2515c28e
Dayle Clarke <dayleclarke07@gmail.com>
2023-02-04 23:17:47 +1000

    Updated error handling on deleteloan route

--- src/backend/controllers/bookDetailsController.js
+++ src/backend/controllers/bookDetailsController.js
@@ -1,6 +1,5 @@
 const asyncHandler = require("express-async-handler")
 const bookCopyModel = require("../models/bookCopyModel")
-
 const BookDetailsModel = require("../models/bookDetailsModel")
 
 // @desc    Get all book details
@@ -11,9 +10,7 @@ const getBookDetails = asyncHandler(async (req, res) => {
   res.status(200).json(bookDetails)
 })
 
-// To-Do- Find out why the else statement below doesn't work but the catch does.
-
-// @desc    Get a single book's details by its ID
+// @desc    Get a single book's details by its ID passed in as a URI parameter
 // @route   GET /api/bookdetails/:id
 // @access  Public
 const getOneBookDetails = asyncHandler(async (req, res) => { 
@@ -25,17 +22,11 @@ const getOneBookDetails = asyncHandler(async (req, res) => {
 
       res.status(200).json({...bookDetails.toJSON(), availableCopies:count})
     }
-    // else {
-    //   res.status(404).send({ error: 'Book copy not found with that ID'})
-    // }
   }
   catch (err) {
     res.status(404).send({ error: "Book details not found with that ID" })
   }
   })
-
-// To-do Add functionality to prevent someone adding the same book twice (not allowing duplicate title and author) 
-
 
 // @desc    Set new book details
 // @route   POST /api/bookdetails
@@ -52,9 +43,6 @@ const setBookDetails = asyncHandler(async (req, res) => {
   res.status(200).json(bookDetails)
 })
 
-//To-Do: find a way to provide a more semantic error message for then a book doesn't exist with that id. 
-// Protected route (To-Do add isAdmin)
-
 // @desc    Update an exsisting book's details
 // @route   PUT /api/bookdetails/:id
 // @access  Admin Private
@@ -68,7 +56,7 @@ const updateBookDetails = asyncHandler(async (req, res) => {
   
   if (!bookDetails) {
     res.status(400)
-    throw new Error('No book could be found with that id.')
+    throw new Error(`No book could be found with id: ${req.params.id}.`)
   }
   const updatedBookDetails = await BookDetailsModel.findByIdAndUpdate(req.params.id, req.body, {
         new:true,
@@ -89,7 +77,7 @@ const deleteBookDetails = asyncHandler(async (req, res) => {
   const bookDetails = await BookDetailsModel.findById(req.params.id)
   if (!bookDetails) {
     res.status(400)
-    throw new Error('No book could be found with that id.')
+    throw new Error(`No book could be found with id ${req.params.id}.`)
   }
   
   await bookDetails.remove()
